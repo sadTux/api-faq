d80212bfa0b256875c11ff50005de5e145d00844
Ronaldo Nascimento <ronaldonascimento977@gmail.com>
2022-06-29 22:50:27 -0400

    Fix: Adiciona CORS

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 from .routes.auth import apiRouter as routerAuth
 from .routes.user import apiRouter as routerUser
 from .routes.question import apiRouter as routerQuestion
-
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routerAuth, prefix="/auth", tags=["login"])
 app.include_router(routerUser, prefix="/user", tags=["user"])

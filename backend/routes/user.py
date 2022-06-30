480e48ab01598f36e631e55aaa5f7ee4e111a9ac
Ronaldo Nascimento <ronaldonascimento977@gmail.com>
2022-06-29 22:40:06 -0400

    Feat: integra funcao de criacao de usuario

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from backend.schemas.usuario import criaUsuario as Usuario
+from backend.crud.usuario import criaUsuario
+from backend.db.sessao import obtem_DB
+from sqlalchemy.orm import Session
 
 apiRouter = APIRouter()
 
 @apiRouter.post("/")
-def createUser():
+def createUser(usuario: Usuario, db: Session = Depends(obtem_DB)):
+  criaUsuario(db=db, usuario=usuario)
   return {"msg": "Created"}
 
 @apiRouter.delete("/{id}")

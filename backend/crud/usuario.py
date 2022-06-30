a18b19caa71a6182066d0ae4e9de17e4e3cc3b8e
Ronaldo Nascimento <ronaldonascimento977@gmail.com>
2022-06-29 22:38:50 -0400

    Fix: Corrige rota de criacao de usuario

--- backend/crud/usuario.py
+++ backend/crud/usuario.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import Session
-from backend.schemas.usuario import criaUsuario
+from backend.schemas.usuario import criaUsuario as Usuario
 from backend.models.usuario import Usuario
 
-def criaUsuario(db:Session, usuario: criaUsuario):
+def criaUsuario(db:Session, usuario: Usuario):
     db_usuario = Usuario(email = usuario.email, nome = usuario.nome, apelido = usuario.apelido, senha = usuario.senha)
     db.add(db_usuario)
     db.commit()

f2a73a8d619a3bce356a536b687cece800bb619d
Ronaldo Nascimento <ronaldonascimento977@gmail.com>
2022-06-29 22:39:28 -0400

    Feat: Cria funcao de geracao de sessao

--- backend/db/sessao.py
+++ backend/db/sessao.py
@@ -1,7 +1,10 @@
-from enum import auto
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from backend.config import configuracoes
 
 motor = create_engine(configuracoes.DB_URL, pool_pre_ping = True)
 sessao = sessionmaker(bind=motor, autoflush= False, autocommit = False)
+
+def obtem_DB():
+    with sessao() as s:
+        yield s

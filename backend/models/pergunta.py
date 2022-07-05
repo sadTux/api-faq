8f78f2c0f117da1069e8147ed650357e769834bc
Ronaldo Nascimento <ronaldonascimento977@gmail.com>
2022-07-04 21:39:42 -0400

    fix: remove campo titulo da tabela de perguntas.

--- backend/models/pergunta.py
+++ backend/models/pergunta.py
@@ -4,6 +4,5 @@ from backend.db.base_class import Base
 class Pergunta(Base):
     __tablename__ = "pergunta"
     id = Column(Integer, primary_key = True)
-    titulo = Column(String, nullable = False)
     texto = Column(Text, nullable = False)
     usuario_id = Column(Integer, ForeignKey("usuario.id"), nullable = False)
